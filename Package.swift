d882c9805c998c1715653399c68e3329ddefc50e
Jay Herron <NeedleInAJayStack@protonmail.com>
2024-11-28 00:01:41 -0700

    simplifies library names

--- Package.swift
+++ Package.swift
@@ -8,17 +8,17 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SpmCacheRecreatorA",
-            targets: ["SpmCacheRecreatorA"]),
+            name: "A",
+            targets: ["A"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SpmCacheRecreatorA"),
+            name: "A"),
         .testTarget(
             name: "SpmCacheRecreatorATests",
-            dependencies: ["SpmCacheRecreatorA"]
+            dependencies: ["A"]
         ),
     ]
 )
